050604c9e50f64b6f5f7584365df62849cce7e99
Clement Allen <clementallen313@gmail.com>
2015-07-03 10:34:51 +0100

    Added separate css minification job

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
 var gulp = require('gulp'),
     compass = require('gulp-compass'),
     minifyCSS = require('gulp-minify-css'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    rename = require("gulp-rename");
 
 // Build sass into css
 gulp.task('styles', function() {
-  gulp.src('./sources/sass/main.scss')
+  return gulp.src('./sources/sass/main.scss')
     .pipe(compass({
-      css: 'public/css',
-      sass: 'sources/sass'
+      css: './public/css',
+      sass: './sources/sass'
     }))
-    .pipe(minifyCSS())
     .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('minify', function() {
+    return gulp.src('./public/css/main.css')
+    .pipe(minifyCSS())
+    .pipe(rename(function (path) {
+    path.basename  = "main.min"
+    }))
+    .pipe(gulp.dest('./public/css'))
 });
 
 // Watch sass files for changes and then build
@@ -19,5 +28,5 @@ gulp.task('watch', function () {
    gulp.watch('./sources/sass/**/*.scss', ['styles']);
 });
 
-gulp.task('build', ['styles']);
+gulp.task('build', ['styles', 'minify']);
 gulp.task('default', ['build']);
